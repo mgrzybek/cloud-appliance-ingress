1259c1e40f0d00ff0dce9b02d2bc944f004b4357
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2020-05-13 10:55:44 +0200

    [autoconf] set https_proxy for autreconf

--- ingress.appliance.autoconf.sh
+++ ingress.appliance.autoconf.sh
@@ -4,8 +4,10 @@ cat > /root/appliance/ingress.appliance.reconf.sh <<EOF
 export ETC_PATH=$ETC_PATH
 export REPO_PATH=$REPO_PATH
 
+export HTTPS_PROXY=$HTTPS_PROXY
 export HTTP_PROXY=$HTTP_PROXY
 export NO_PROXY=$NO_PROXY
+export https_proxy=$https_proxy
 export http_proxy=$http_proxy
 export no_proxy=$no_proxy
 
