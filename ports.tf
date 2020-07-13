3cc703e2d62720fef6d364b840ca737c0ab1f3aa
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2020-07-13 14:33:43 +0200

    [secgroup] allow http/https from front office port

--- ports.tf
+++ ports.tf
@@ -4,7 +4,8 @@
 resource "openstack_networking_port_v2" "appliance-ingress-front-port" {
   name = "appliance-ingress-front-port"
   security_group_ids = [
-    var.default_secgroup_id
+    var.default_secgroup_id,
+    openstack_networking_secgroup_v2.appliance-ingress-secgroup.id
   ]
   network_id = var.front_net_id
 }
