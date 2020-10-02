13682072c3ce800018e8d4db404b3b5e77f3f860
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2020-10-02 14:04:18 +0200

    [netdata] use apt package

--- appliance.tf
+++ appliance.tf
@@ -88,3 +88,13 @@ resource "openstack_networking_secgroup_rule_v2" "appliance-ingress-secgroup-htt
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.appliance-ingress-secgroup.id
 }
+
+resource "openstack_networking_secgroup_rule_v2" "appliance-ingress-secgroup-netdata" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 19999
+  port_range_max    = 19999
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.appliance-ingress-secgroup.id
+}
